6843ba9e2937cd6943fed98152a30ea70a63b2c0
Nitu Dubey <108494260+Nitu170287@users.noreply.github.com>
2023-06-12 18:54:43 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species = species
+	}
+	makeSound(){
+		return this.species+" make sound"
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	constructor(){
+		super("dog")
+	}
+	bark(){
+		console.log(this.makeSound() +" woof")
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	constructor(){
+		super("cat")
+	}
+	purr(){
+		console.log(this.makeSound() +" purr")
+	}
+}
+
+let dog =new Dog();
+dog.bark()
+let cat =new Cat();
+cat.purr()
 
 // Do not change the code below this line
 window.Animal = Animal;
